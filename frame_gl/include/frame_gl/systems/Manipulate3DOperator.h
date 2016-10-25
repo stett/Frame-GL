9c5dad1d591b74592e17b8d3d39cfbb4ab2a0608
Stett <stettjawa@gmail.com>
2016-10-25 00:15:07 -0700

    sweet meaningless nothings

--- frame_gl/include/frame_gl/systems/Manipulate3DOperator.h
+++ frame_gl/include/frame_gl/systems/Manipulate3DOperator.h
@@ -16,6 +16,9 @@ namespace frame
     FRAME_SYSTEM(Manipulate3DOperator, Node<Transform, Manipulate3D>) {
 
     public:
+        Manipulate3DOperator() : _ui_text_color(1.0f, 0.5f, 0.25f, 1.0f) {}
+
+    public:
         void step(float dt) {
 
             auto input = frame()->systems().get<Input>();
@@ -33,11 +36,11 @@ namespace frame
                 if (input->mouse_down(manipulate->button())) {
 
                     // Add control prompts
-                    ui_text->add_line("Zoom        - Mouse Wheel", ui_text_color);
-                    ui_text->add_line("Move        - W/A/S/D", ui_text_color);
-                    ui_text->add_line("Move Fast   - Left Shift", ui_text_color);
-                    ui_text->add_line("Raise/Lower - E/Q", ui_text_color);
-                    ui_text->add_line("Re-center   - Space", ui_text_color);
+                    ui_text->add_line("Zoom              Mouse Wheel", ui_text_color);
+                    ui_text->add_line("Move              W/A/S/D", ui_text_color);
+                    ui_text->add_line("Move Fast         Left Shift", ui_text_color);
+                    ui_text->add_line("Raise/Lower       E/Q", ui_text_color);
+                    ui_text->add_line("Re-center         Space", ui_text_color);
 
                     // Angle manipulation
                     manipulate->shift_angle(input->mouse_delta(), -10.0f * input->mouse_scroll().y);
@@ -58,7 +61,7 @@ namespace frame
                 } else {
 
                     // Add mouse control prompt
-                    ui_text->add_line(std::string("Manipulate view - ") + (manipulate->button() == Input::MouseButton::Left ? "Left Mouse Button" : "Right Mouse Button"), ui_text_color);
+                    ui_text->add_line(std::string("Manipulate view   ") + (manipulate->button() == Input::MouseButton::Left ? "Left Mouse Button" : "Right Mouse Button"), ui_text_color);
                 }
 
                 // Interpolate motion
@@ -91,5 +94,13 @@ namespace frame
                 transform->set_rotation(manipulate->rotation());
             }
         }
+
+    public:
+
+        const vec4& ui_text_color() { return _ui_text_color; }
+
+    private:
+
+        vec4 _ui_text_color;
     };
 }
