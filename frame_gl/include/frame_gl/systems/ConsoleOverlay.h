1e8ecec34340fe2d19dacb0f02c834ff272e6bb2
Stett <stettjawa@gmail.com>
2017-02-12 17:24:44 -0800

    ok... SORTA works with debugdraw now

--- frame_gl/include/frame_gl/systems/ConsoleOverlay.h
+++ frame_gl/include/frame_gl/systems/ConsoleOverlay.h
@@ -1,10 +1,12 @@
 #pragma once
 #include <sstream>
+#include <ostream>
 #include <unordered_map>
 #include <queue>
 #include "frame/System.h"
 #include "frame/Log.h"
 #include "frame_gl/systems/Input.h"
+#include "frame_gl/systems/DebugDraw.h"
 #include "frame/Frame.h"
 
 namespace frame_gl
@@ -18,6 +20,11 @@ namespace frame_gl
     protected:
 
         void setup() {
+            Log::set_output_stream(output_buffer);
+        }
+
+        void teardown() {
+            Log::reset_output_stream();
         }
 
         void step() {
@@ -68,16 +75,53 @@ namespace frame_gl
                         }
                     }
                 }
+            }
 
-                // TEMP
-                // If input buffer changed, print it
-                if (changed)
-                    Log::input(input_buffer.str());
-                // END TEMP
+            DebugDraw* debug_draw = system<DebugDraw>();
+            if (debug_draw) {
+
+                {
+                    std::string line;
+                    output_buffer.flush();
+
+                    while (std::getline(output_buffer, line)) {
+                        screen_buffer.push_back(line);
+                        if (screen_buffer.size() > 25)
+                            screen_buffer.pop_front();
+                    }
+
+                    //
+                    // TODO: Don't do this, you moron.
+                    //       Just do a custom "safe" getline.
+                    //
+                    // RETORT: Ah, man, but those are so annoying.
+                    //         Why the hell doesn't it just work?
+                    //
+                    output_buffer = std::stringstream();
+                    Log::set_output_stream(output_buffer);
+
+                    auto state = output_buffer.rdstate();
+                    auto flags = output_buffer.flags();
+                    auto good = output_buffer.good();
+                }
+
+                {
+                    glm::vec2 pos(10.0f);
+                    for (auto& line : screen_buffer) {
+                        debug_draw->screen_text(pos + vec2(1.0f), line, vec4(vec3(0.0f), 1.0f), 16.0f);
+                        debug_draw->screen_text(pos, line, vec4(1.0f), 16.0f);
+                        pos.y += 16.0f;
+                    }
+
+                    debug_draw->screen_text(pos + vec2(1.0f), input_buffer.str(), vec4(vec3(0.0f), 1.0f), 16.0f);
+                    debug_draw->screen_text(pos, input_buffer.str(), vec4(1.0f, 0.5f, 0.5f, 1.0f), 16.0f);
+                }
             }
         }
 
     private:
+        std::list<std::string> screen_buffer;
+        std::stringstream output_buffer;
         std::stringstream input_buffer;
         std::unordered_map< char, float > key_timer;
         float key_timeout;
